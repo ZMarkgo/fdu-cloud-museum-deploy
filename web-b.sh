f1c4dee0464208aab997f77531467e583182487d
ZMark <351479188@qq.com>
2023-03-22 23:28:41 +0800

    fix:shell的update功能修复

--- web-b.sh
+++ web-b.sh
@@ -6,13 +6,14 @@ URL="http://10.177.35.97:8085"
 #使用说明，用来提示输入参数
 usage() {
   echo "Usage: ./web-b-front.sh [start|stop|restart|status|update]"
-  echo " Update：丢弃所有的本地改动，强制使用origin版本，然后restart"
+  echo " - restart：分别重启前后端"
+  echo " - update：丢弃所有的本地改动，强制使用origin版本，然后restart"
   echo ">>> 如果启动成功，可以尝试访问：${URL}"
   exit 1
 }
 
 start(){
-  echo ">>> web-b 后端通过jar包启动"
+	echo ">>> web-b 后端通过jar包启动"
 	cd web-b-rear
 	chmod +x ./web-b-rear.sh
 	./web-b-rear.sh start
@@ -56,14 +57,6 @@ restart(){
 update(){
 	git fetch origin
 	git reset --hard origin/master
-	
-	cd web-b-rear
-	chmod +x ./web-b-rear.sh
-	./web-b-rear.sh restart
-
-	cd ../web-b-front
-	chmod +x ./web-b-front.sh 
-	./web-b-front.sh update
 }
 
 #根据输入参数，选择执行对应方法，不输入则执行使用说明
@@ -81,7 +74,7 @@ case "$1" in
     restart
     ;;
   "update")
-    update_files
+    update
 	restart
     ;;
   *)
