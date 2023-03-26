3948dae836be1792b1058ede5a5c9d83ee170896
ZMark <351479188@qq.com>
2023-03-26 15:50:40 +0800

    fix:网站a的启动脚本后台运行

--- web-a.sh
+++ web-a.sh
@@ -5,5 +5,4 @@ URL="http://10.177.35.97:4000"
 LOG_FILE="/home/david/develop/fdu-cloud-museum/fdu-cloud-museum-deploy/log/runyarn.out"
 
 cd /home/david/develop/mynerfstudio/nerfstudio/viewer/app/
-yarn start
 nohup yarn start > $LOG_FILE 2>&1 &
