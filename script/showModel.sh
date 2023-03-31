2a40188b79637ce6f552e2360de37c945e3f3902
ZMark <351479188@qq.com>
2023-03-31 20:15:16 +0800

    fix(sh):模型展示

--- script/showModel.sh
+++ script/showModel.sh
@@ -10,7 +10,8 @@ function on_sigterm() {
 trap on_sigterm SIGTERM
 
 # 设置日志路径
-logPath="/home/david/develop/fdu-cloud-museum/fdu-cloud-museum-deploy/log/show_model_sh.out"
+ns_train_out="/home/david/develop/fdu-cloud-museum/fdu-cloud-museum-deploy/log/ns-train.out"
+sh_out="/home/david/develop/fdu-cloud-museum/fdu-cloud-museum-deploy/log/show_model_sh.out"
 # 设置工作目录
 work_dir="/home/david/develop/mynerfstudio"
 # ns-train的训练参数
@@ -25,31 +26,31 @@ PORT="7007"
 PID=$(lsof -t -i:$PORT)
 
 if [[ -z $PID ]]; then
-  echo "端口 $PORT 没有被占用"
+  echo "端口 $PORT 没有被占用" >> ${sh_out}
 else
   # 结束占用端口的进程
   kill -9 $PID
-  echo "成功杀死进程 $PID，释放端口 $PORT"
+  echo "成功杀死进程 $PID，释放端口 $PORT" >> ${sh_out}
 fi
 
 cd ${work_dir}
 source activate
 conda activate nerfstudio
-echo $?
+echo $? >> ${sh_out}
 
 if [ $data_path = $dockey_data_path ]; then
 	nohup ns-train nerfacto \
 	--data ${data_path} \
 	--load-dir ${load_dir} \
 	--viewer.start-train False \
-	> ${logPath} 2>&1 &
+	> ${ns_train_out} 2>&1 &
 else
 	nohup ns-train nerfacto \
 	--data ${data_path} \
 	--load-dir ${load_dir} \
 	--viewer.start-train False \
 	--pipeline.model.predict-normals True \
-	> ${logPath} 2>&1 &
+	> ${ns_train_out} 2>&1 &
 fi
 # $! 获取到最后一个在后台执行的进程ID
 # $pid 保存该进程ID
