726c3b76b840295032095698d927af79b4ec18df
ZMark <351479188@qq.com>
2023-03-31 19:52:50 +0800

    fix(shell)

--- script/showModel.sh
+++ script/showModel.sh
@@ -16,6 +16,7 @@ work_dir="/home/david/develop/mynerfstudio"
 # ns-train的训练参数
 data_path=$1 # "data/6"
 load_dir=$2  # "outputs/6/nerfacto/2023-03-17_233144/nerfstudio_models"
+dockey_data_path="data/ala/statue"
 
 # 获取输入参数：端口号
 PORT="7007"
@@ -35,12 +36,21 @@ cd ${work_dir}
 source activate
 conda activate nerfstudio
 echo $?
-nohup ns-train nerfacto \
---data ${data_path} \
---load-dir ${load_dir} \
---viewer.start-train False \
-> ${logPath} 2>&1 &
 
+if [ $data_path = $dockey_data_path ]; then
+	nohup ns-train nerfacto \
+	--data ${data_path} \
+	--load-dir ${load_dir} \
+	--viewer.start-train False \
+	> ${logPath} 2>&1 &
+else
+	nohup ns-train nerfacto \
+	--data ${data_path} \
+	--load-dir ${load_dir} \
+	--viewer.start-train False \
+	--pipeline.model.predict-normals True \
+	> ${logPath} 2>&1 &
+fi
 # $! 获取到最后一个在后台执行的进程ID
 # $pid 保存该进程ID
 # 使用 wait 命令等待该进程执行完毕
